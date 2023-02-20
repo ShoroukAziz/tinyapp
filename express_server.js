38750fee31e97733f2405d3c56d05860ad72bf91
ShoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-02-20 00:02:22 -0700

    add a handler and a vie for  /urls/:id

--- express_server.js
+++ express_server.js
@@ -21,6 +21,11 @@ app.get("/urls", (req, res) => {
 
 });
 
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
+
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
