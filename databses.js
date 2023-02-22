375c9d103f0b76516348161b011aa8d0b00a5b3a
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-02-21 23:21:22 -0700

    remove dummy data

--- databses.js
+++ databses.js
@@ -1,34 +1,5 @@
-const urlDatabase = {
-  "b2xVn2": {
-    longURL: "http://www.lighthouselabs.ca",
-    createdDate: "2023-02-20",
-    userID: "aJ48lW",
-  },
-  "9sm5xK": {
-    longURL: "http://www.google.com",
-    createdDate: "2023-02-21",
-    userID: "aJ48lW",
-  },
-  "9sm5xg": {
-    longURL: "http://www.google.com",
-    createdDate: "2023-02-21",
-    userID: "ah48lk",
-  }
-};
-
-
-const users = {
-  aJ48lW: {
-    id: "aJ48lW",
-    email: "user@example.com",
-    password: "purple-monkey-dinosaur",
-  },
-  ah48lk: {
-    id: "ah48lk",
-    email: "user2@example.com",
-    password: "dishwasher-funk",
-  },
-};
+const urlDatabase = {};
+const users = {};
 
 const errorMessages = {
   emptyEmailOrPassword: "Email and password can't be empty.",
