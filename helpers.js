64ad9d93f0acd58a94507d8da83b782f0eac9f89
shoroukAziz <shorouk.s.abdelaziz@gmail.com>
2023-02-22 21:09:38 -0700

    implement generate user and save user functions

--- helpers.js
+++ helpers.js
@@ -1,7 +1,9 @@
+const bcrypt = require('bcryptjs');
+
 /*
  returns a string of 6 random alphanumeric characters
 */
-const generateRandomString = function() {
+const generateRandomString = function () {
 
   /*toString takes an integer in the range 2 through 36
    specifying the base to use for representing the number value.
@@ -12,7 +14,7 @@ const generateRandomString = function() {
   return Math.random().toString(36).slice(2, 8).toUpperCase();
 };
 
-const findUserByEmail = function(email, userDatabase) {
+const findUserByEmail = function (email, userDatabase) {
 
   for (let userId of Object.keys(userDatabase)) {
     if (userDatabase[userId].email === email) {
@@ -22,7 +24,7 @@ const findUserByEmail = function(email, userDatabase) {
   return null;
 };
 
-const urlsForUser = function(id, urlDatabase) {
+const urlsForUser = function (id, urlDatabase) {
 
   const urls = {};
   for (let urlId of Object.keys(urlDatabase)) {
@@ -33,4 +35,17 @@ const urlsForUser = function(id, urlDatabase) {
   return urls;
 };
 
-module.exports = { generateRandomString, findUserByEmail, urlsForUser };
+const generateNewUser = function (email, password) {
+
+  return {
+    id: generateRandomString(),
+    email,
+    password: bcrypt.hashSync(password, 10)
+  }
+}
+
+const saveUserToDataBase = function (user, userDatabase) {
+  userDatabase[user.id] = user;
+}
+
+module.exports = { generateRandomString, findUserByEmail, urlsForUser, generateNewUser, saveUserToDataBase };
